39fd4c33e479ca565711cd58856fcb5ffb85d3c3
Mayanksain244 <112925358+Mayanksain244@users.noreply.github.com>
2024-10-19 16:50:12 +0530

    ready for mac

--- src/app/processes/ProcessManagement.cpp
+++ src/app/processes/ProcessManagement.cpp
@@ -1,22 +1,74 @@
+#include <windows.h>
 #include "ProcessManagement.hpp"
 #include <iostream>
 #include <cstring>
 #include "../encryptDecrypt/Cryption.hpp"
+#include <atomic>
+#include <sys/fcntl.h>
+#include <semaphore>
 
-ProcessManagement::ProcessManagement(){}
+ProcessManagement::ProcessManagement(){
+    sem_t* itemsSemaphore = sem_open("/itmes_Semaphore",O_CREAT , 0666 ,0);
+    sem_t* emptySlotsSemaphore = sem_open("/empty_slots_semaphore",O_CREAT , 0666 ,1000);
+
+    shmFd = shm_open(SHH_NAME , O_CREAT | O_RDWR , 0666)
+    ftruncate(shmFd , sizeof(SharedMemory));
+    sharedMem = static_cast<SharedMemory *> (mmap(nullptr, sizeof(SharedMemory), PROT_READ | PROT_WRITE , MAP_SHARED . shmFd , 0)
+    sharedMem->front = 0;
+    sharedMem->rear = 0;
+    sharedMem->size.store(0);
+}
+
+~ProcessManagement(){
+    unmap(sharedMem, sizeof(SharedMemory));
+    shm_unlink(SHH_NAME);
+}
+
 
 bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task){
-    taskQueue.push(std::move(task));
+    sem_wait(emptySlotsSemaphore);
+    std::unique_lock<std::mutex> queueLock;
+
+    if sharedMem->size.load() >= 1000{
+        return false;
+    }
+
+    strcpy(shareMem->task[sharedMem->rear], task->tostring().c_str());
+    sharedMem->rear = (sharedMem->rear + 1) % 1000;
+    sharedMem->size.fetch_add(1);
+
+    lock.unlock();
+    sem_post(itemsSemaphore)
+
+    // taskQueue.push(std::move(task));
+    int pid = fork();
+    if(pid<0){
+        return false;
+    }else if(pid>0){
+        std::cout<<"Entering the parent process"<<std::endl;
+    }else{
+        std::cout<<"Entering the child process"<<std::endl;
+        executeTasks();
+        std::cout<<"Exiting the child process"<<std::endl;
+        exit(0);
+    }
     return true;
 }
 
 void ProcessManagement::executeTasks(){
-    while (!taskQueue.empty())
-    {
-        std::unique_ptr<Task> taskToExecute = std::move(taskQueue.front());
-        taskQueue.pop();
-        std::cout<<"Executing task : "<<taskToExecute->toString()<<std::endl;
-        executeCryption(taskToExecute->toString());
-    }
+    sem_wait(itemsSemaphore);
+    std::unique_lock<std::mutex> queueLock;
+
+    char taskStr[256];
+    strcpy(taskStr,sharedMem->tasks[sharedMem->front])
+    sharedMem->front = (sharedMem->front + 1) % 1000;
+    sharedMem->size.fetch_sub(1);
+
+    lock.unlock();
+    sem_post(emptySlotsSemaphore);
+
+
+    std::cout<<"Executing child process"<<std::endl;
+    executeCryption(taskStr);
     
 }
--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -5,13 +5,35 @@
 #include "Task.hpp"
 #include <queue>
 #include <memory>
+#include <atomic>
+#include <semaphore>
 
 class ProcessManagement{
+    sem_t* itemsSemaphore;
+    sem_t* emptySemaphore;
 private:
-    std::queue<std::unique_ptr<Task>> taskQueue;
+    struct SharedMemory
+    {
+        std::atomic<int> size;
+        char tasks[1000][256];
+        int front;
+        int rear;
+
+        void printSharedMemory(){
+            std::cout<<size<<std::endl;
+        }
+    };
+
+    SharedMemory* sharedMem;
+    int shmFd;
+    const char* SHH_NAME = "/my_queue";
+    std::mutex queueLock;
+    
+    // std::queue<std::unique_ptr<Task>> taskQueue;
 
 public:
     ProcessManagement();
+    ~ProcessManagement();
     bool submitToQueue(std::unique_ptr<Task> task);
     void executeTasks();
 
